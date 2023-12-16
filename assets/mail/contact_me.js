0dbc9ed44c47c537dbe4576b7eb41e9d5db112bf
Yusup Firmansyah <yhusup6@gmail.com>
2023-12-17 02:55:30 +0700

    Update Portfolio

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -19,57 +19,58 @@ $(function () {
                 firstName = name.split(" ").slice(0, -1).join(" ");
             }
             $this = $("#sendMessageButton");
-            $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-            $.ajax({
-                url: "/assets/mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message,
-                },
-                cache: false,
-                success: function () {
-                    // Success message
-                    $("#success").html("<div class='alert alert-success'>");
-                    $("#success > .alert-success")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
-                    $("#success > .alert-success").append(
-                        "<strong>Your message has been sent. </strong>"
-                    );
-                    $("#success > .alert-success").append("</div>");
-                    //clear all fields
-                    $("#contactForm").trigger("reset");
-                },
-                error: function () {
-                    // Fail message
-                    $("#success").html("<div class='alert alert-danger'>");
-                    $("#success > .alert-danger")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
-                    $("#success > .alert-danger").append(
-                        $("<strong>").text(
-                            "Maaf " +
-                                firstName +
-                                ", Fitur ini belum bisa digunakan. Silahkan klik icon sosial media dibawah untuk menghubungi saya!"
-                        )
-                    );
-                    $("#success > .alert-danger").append("</div>");
-                    //clear all fields
-                    $("#contactForm").trigger("reset");
-                },
-                complete: function () {
-                    setTimeout(function () {
-                        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-                    }, 1000);
-                },
-            });
+            // $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+            window.location = "mailto:deb@empirerecords.biz?subject=Blog Feedback";
+            // $.ajax({
+            //     url: "mailto:deb@empirerecords.biz?subject=Blog Feedback",
+            //     type: "POST",
+            //     data: {
+            //         name: name,
+            //         phone: phone,
+            //         email: email,
+            //         message: message,
+            //     },
+            //     cache: false,
+            //     success: function () {
+            //         // Success message
+            //         $("#success").html("<div class='alert alert-success'>");
+            //         $("#success > .alert-success")
+            //             .html(
+            //                 "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+            //             )
+            //             .append("</button>");
+            //         $("#success > .alert-success").append(
+            //             "<strong>Your message has been sent. </strong>"
+            //         );
+            //         $("#success > .alert-success").append("</div>");
+            //         //clear all fields
+            //         $("#contactForm").trigger("reset");
+            //     },
+            //     error: function () {
+            //         // Fail message
+            //         $("#success").html("<div class='alert alert-danger'>");
+            //         $("#success > .alert-danger")
+            //             .html(
+            //                 "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+            //             )
+            //             .append("</button>");
+            //         $("#success > .alert-danger").append(
+            //             $("<strong>").text(
+            //                 "Maaf " +
+            //                 firstName +
+            //                 ", Fitur ini belum bisa digunakan. Silahkan email ke yhusup6@gmail.com atau klik icon sosial media dibawah untuk menghubungi saya!"
+            //             )
+            //         );
+            //         $("#success > .alert-danger").append("</div>");
+            //         //clear all fields
+            //         $("#contactForm").trigger("reset");
+            //     },
+            //     complete: function () {
+            //         setTimeout(function () {
+            //             $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+            //         }, 1000);
+            //     },
+            // });
         },
         filter: function () {
             return $(this).is(":visible");
